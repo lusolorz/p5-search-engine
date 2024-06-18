363c818701e4ecc64f0111eeb503da83515d1765
Luciana Solorzano <luciana@brightvessel.com>
2024-06-17 20:12:29 -0400

    l

--- inverted_index/reduce3.py
+++ inverted_index/reduce3.py
@@ -2,15 +2,24 @@
 """Reduce 3."""
 import sys
 import itertools
+import math
 
 # answer should be term: tf-idf score, doc_id, tf fin doc, .....
+# term 
 
-
+#add these all to the same line:
 def reduce_one_group(key, group):
     # get term frequency accross all docs here 
-    # all_docs = len(group)
+    # we can also add to a string and return that
+    # and thne deal with it in map with a 4 for loop every four return 4 
+    temp = list(group)
+    all_docs = len(temp)
     """Reduce one group."""
-    # for line in group:
+    for line in group:
+        doc_id = line.partition("\t")[2]
+        nt = line.partition("\t")[4]
+        idf_score = math.log10(all_docs/nt)
+        print(f"{key}\t{idf_score}\t{doc_id}\t{nt}")
         # should be grouped by term now
         # get term frequency accross all docs here 
         # add the overall term freq 
